038f8a6603fcd31578a9bd7d726c29679beca17f
Elia Sarti <elia@eligt.com>
2022-09-25 02:29:26 +0100

    Update extending sample for new features

--- samples/extending.cpp
+++ samples/extending.cpp
@@ -20,7 +20,7 @@ enum class DocumentType
 	IMAGE,
 	WAVE,
 	INHERITANCE,
-	EXTENSION = 128, // reserve the first 128 values to buil-in and inherited
+	EXTENSION = 128, // reserve the first 127 values to built-in and inherited
 	MAX = 255 // support at maximum 255 document types
 };
 
@@ -54,7 +54,7 @@ enum class DocumentType_Extended
 	RICHTEXT = EnumeratorMeta<DocumentType>::Inheritor::inherit(),
 	SPREADSHEET,
 	VIDEO,
-	MAX = 255 // support at maximum 255 document types
+	MAX = EnumeratorMeta<DocumentType>::Inheritor::inheritMaximum()
 };
 
 template <>
@@ -62,16 +62,24 @@ class EnumeratorMeta<DocumentType_Extended> : public EnumeratorMetaDefault<Docum
 {
 public:
 	using BaseEnumType = DocumentType;
+	using EnumEntry = EnumeratorMeta<DocumentType>::EnumEntry;
 	
 	static constexpr const bool logic_operators = true;
 	static constexpr const bool string_operators = true;
 	static constexpr const DocumentType_Extended MAX_VALUE = DocumentType_Extended::MAX;
 	
 	static constexpr const EnumEntry enum_entries[]{
-		{ DocumentType_Extended::RICHTEXT, "RICHTEXT" },
-		{ DocumentType_Extended::SPREADSHEET, "SPREADSHEET" },
-		{ DocumentType_Extended::VIDEO, "VIDEO" }
+		{ +DocumentType_Extended::RICHTEXT, "RICHTEXT" },
+		{ +DocumentType_Extended::SPREADSHEET, "SPREADSHEET" },
+		{ +DocumentType_Extended::VIDEO, "VIDEO" }
 	};
+};
+
+template <>
+class EnumeratorInherited<DocumentType>
+{
+public:
+	using InheritedType = DocumentType_Extended;
 };
 
 constexpr EnumeratorMeta<DocumentType_Extended>::EnumEntry EnumeratorMeta<DocumentType_Extended>::enum_entries[];
@@ -92,13 +100,18 @@ int main(int argc, char *argv[])
 	// prints: Support for VIDEO document enabled: 1
 	
 	// Extending the enum at run-time, to record dynamic document types (for instance, added by plugins or external modules)
-	auto documentType1 = EnumeratorMeta<DocumentType>::Extender::extend();
-	auto documentType2 = EnumeratorMeta<DocumentType>::Extender::extend();
+	using Extender = EnumeratorMeta<DocumentType>::Extender;
+	auto documentType1 = Extender::extend("documentType1");
+	auto documentType2 = Extender::extend("documentType2");
 	DocumentTypeMask pluginTypes = documentType1 | documentType2;
 	std::cout << "Is " << DocumentType_Extended::RICHTEXT << " registered by plugin: " << pluginTypes.has(+DocumentType_Extended::RICHTEXT) << "\n";
 	// prints: Is RICHTEXT registered by plugin: 0
-	std::cout << "Is documentType1 registered by plugin: " << pluginTypes.has(documentType1) << "\n";
+	std::cout << "Is " << documentType1 << " registered by plugin: " << pluginTypes.has(documentType1) << "\n";
 	// prints: Is documentType1 registered by plugin: 1
+	
+	using Serializer = EnumeratorSerializer<DocumentType>;
+	std::cout << "Unserialized values " << (int)Serializer::get_value("documentType1") << ":" << Serializer::get_value("documentType1") << ", " << (int)Serializer::get_value("RICHTEXT") << ":" <<Serializer::get_value("RICHTEXT") << ", " << (int)Serializer::get_value("WAVE") << ":" <<Serializer::get_value("WAVE");
+	// prints: Unserialized values 128:documentType1, 4:RICHTEXT, 3:WAVE
 	
 	std::cout << "\n";
 }
